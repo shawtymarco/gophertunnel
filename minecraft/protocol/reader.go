e17e5bdd9f18cd36c969f09d4b103bd128a5dbb9
marcolol <150941290+shawtymarco@users.noreply.github.com>
2026-08-25 18:31:36 +0900

    feat: promote Minecraft 1.26.50 protocol groundwork (#2)
    * gophertunnel: Start updating for 26.50
    
    * protocol/sub_chunk.go: Correct slice length
    
    * protocol/reader.go: Remove unused import
    
    ---------
    
    Co-authored-by: Seb <twistedasylummc@gmail.com>

--- minecraft/protocol/reader.go
+++ minecraft/protocol/reader.go
@@ -245,9 +245,7 @@ func (r *Reader) PlayerInventoryAction(x *UseItemTransactionData) {
 	OptionalFunc(r, &x.LegacySetItemSlots, func(slots *[]LegacySetItemSlot) {
 		Slice(r, slots)
 	})
-	DoubleOptionalFunc(r, &x.Actions, func(actions *[]InventoryAction) {
-		Slice(r, actions)
-	})
+	Slice(r, &x.Actions)
 	IntegerFunc(&x.ActionType, r.Varint32)
 	IntegerFunc(&x.TriggerType, r.Uint8)
 	r.BlockPos(&x.BlockPosition)
@@ -587,6 +585,10 @@ func (r *Reader) PackSetting(x *PackSetting) {
 	case PackSettingTypeString:
 		var v string
 		r.String(&v)
+		x.Value = v
+	case PackSettingTypeStringList:
+		var v []string
+		FuncSlice(r, &v, r.String)
 		x.Value = v
 	default:
 		r.UnknownEnumOption(t, "pack setting")
